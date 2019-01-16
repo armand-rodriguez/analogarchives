1240e180e5c0570419f5c825807a954e9c536573
Armand <armand.j.rodriguez@gmail.com>
2019-01-16 01:21:19 -0800

    fix seed validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(first_name: 'analog', last_name: 'archives', email: 'armand.j.rodriguez@gmail.com', password: 'H993E2fore v;er', password_confirmation: 'asdasd', admin: true, is_guest: false)
-User.create!(first_name: 'user1first', last_name: 'user2last', email: 'testuser2@test.com', password: 'asdasd', password_confirmation: 'asdasd', admin: false, is_guest: false)
-User.create!(first_name: 'user1first', last_name: 'user2last', email: 'asd@asd.com', password: 'asdasd', password_confirmation: 'asdasd', admin: false, is_guest: false)
+User.create!(first_name: 'analog', last_name: 'archives', email: 'armand.j.rodriguez@gmail.com', password: 'H993E2fore v;er', password_confirmation: 'H993E2fore v;er', admin: true, is_guest: false)
+User.create!(first_name: 'user1first', last_name: 'user2last', email: 'testuser2@test.com', password: 'hoLLy73 ;;form@T', password_confirmation: 'hoLLy73 ;;form@T', admin: false, is_guest: false)
+
 Product.create!(artist: 'test artist 1', album: 'test album 1', description: 'this is a test description for artist 1.', price: 420)
 Product.create!(artist: 'test artist 2', album: 'test album 2', description: 'this is a test description for artist 2.', price: 200)
 Product.create!(artist: 'test artist 3', album: 'test album 3', description: 'this is a test description for artist 3.', price: 550)
