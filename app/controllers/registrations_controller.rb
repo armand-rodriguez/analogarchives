51989870ce605046d8939da99fb1d7f6c398db4c
Armand <armand.j.rodriguez@gmail.com>
2019-01-16 17:37:44 -0800

    undo change to registratonsController

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   # POST /resource/sign_up
   def create
     super
-    current_or_guest_user
-    current_user.update_attributes(is_guest: false)
       @cart.update_attributes(user_id: current_user.id)
       # @guest.update_attributes(cart_id: nil)
-
+      current_or_guest_user
+      current_user.update_attributes(is_guest: false)
   end
 
 private
