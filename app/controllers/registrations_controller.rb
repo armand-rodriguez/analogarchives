98d9458c70e4805389c55a4f634f22e207561c9c
Armand <armand.j.rodriguez@gmail.com>
2019-01-18 12:52:43 -0800

    attempt to fix bug registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,13 @@ class RegistrationsController < Devise::RegistrationsController
   # POST /resource/sign_up
   def create
     super
-      @cart.update_attributes(user_id: current_user.id)
-      # @guest.update_attributes(cart_id: nil)
+      
       current_or_guest_user
       current_user.update_attributes(is_guest: false)
+      @cart.update_attributes(user_id: current_user.id)
+      # @guest.update_attributes(cart_id: nil)
+
+
   end
 
 private
