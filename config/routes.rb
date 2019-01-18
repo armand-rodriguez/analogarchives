3c186c097cc0b9439a6b82b265b1b25dfe428824
Armand <armand.j.rodriguez@gmail.com>
2019-01-18 12:31:36 -0800

    configure devise confirmation mailer

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :orders, only: [:index, :show, :new, :create]
   resources :payments, only: [:new, :create]
-  devise_for :users, :controllers => { registrations: 'registrations', sessions: 'users/sessions' }
+  devise_for :users, :controllers => { registrations: 'registrations', sessions: 'users/sessions', confirmations: 'confirmations' }
   # devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home/contact'
