68fff55349cffe569023d6cb05d9f8935497295c
armand-rodriguez <37813635+armand-rodriguez@users.noreply.github.com>
2019-05-01 18:22:15 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create!(first_name: 'analog', last_name: 'archives', email: 'armand.j.rodriguez@gmail.com', password: 'H993E2fore v;er', password_confirmation: 'H993E2fore v;er', admin: true, is_guest: false)
-user = User.new(first_name: 'analog', last_name: 'archives', email: 'armand.j.rodriguez@gmail.com', password: 'H993E2fore v;er', password_confirmation: 'H993E2fore v;er', admin: true, is_guest: false)
-user.skip_confirmation!
-user.save!
-User.create!(first_name: 'user1first', last_name: 'user2last', email: 'testuser2@test.com', password: 'hoLLy73 ;;form@T', password_confirmation: 'hoLLy73 ;;form@T', admin: false, is_guest: false)
-user.skip_confirmation!
-user.save!
+
 
 Product.create!(artist: 'test artist 1', album: 'test album 1', description: 'this is a test description for artist 1.', price: 420)
 Product.create!(artist: 'test artist 2', album: 'test album 2', description: 'this is a test description for artist 2.', price: 200)
